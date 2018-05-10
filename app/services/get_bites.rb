396e12f80a12b6f8a536773abb6283e8d8776ea4
Arthur Peterson and Emily Watkins <emily@>
2018-05-10 15:20:43 -0700

    refactor to account for versioning.

--- app/services/get_bites.rb
+++ app/services/get_bites.rb
@@ -2,7 +2,7 @@ class AnimalBites
   def self.get_bites
     response = RestClient::Request.execute(
       method: :get,
-      url: 'http://localhost:3000/bites',
-      headers: {Authorization: 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo0LCJleHAiOjE1MjU5ODkwNDd9.SBR8Etphvv8LEu5sFk65nm_IbrW1C-ROTSi0clHYtzM'})
+      url: 'http://localhost:3000/api/v2/bites',
+      headers: {Authorization: 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo0LCJleHAiOjE1MjYwNzY4Njh9.xK13P01rbf_UE9W39wRe4EhlQOiiTBO2dRo5CXQ0COs'})
   end
 end
